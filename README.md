5d542837606b098c6c6ea34c587a83468238a05d
Kat Hicks <kmhicks92@gmail.com>
2016-12-13 14:20:12 +0000

    Added in a new version of the fizzbuzz method as an instance method of the Fixnum class

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Pair programming for Makers Pre-Course Week 4
 - Used RSpec testing - with guidance [here](https://github.com/makersacademy/pre_course/blob/master/pills/rspec.md)
 - You can run a RSpec file using the command: `$ rspec spec/test_spec.rb`
 - Error messages remain the same between different `require` syntax in RSpec file
+- Writing as an instance method: replace all the references to the 'number' argument with `self` within the `::Fixnum` class **and** remove the reference to the argument in the method definition - so `def fizzbuzz(number)` changes to `def fizzbuzz`
--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,5 @@
+# standard method notation
+
 def fizzbuzz(number)
     if number % 5 == 0 && number % 3 == 0
         "fizzbuzz"
@@ -7,5 +9,22 @@ def fizzbuzz(number)
         "fizz"
     else 
         number
+    end
+end
+
+# when working the string class you might write: string.split / split(string)
+# reason is that string object and split method are both members of the string class
+
+class ::Fixnum
+    def morefizzbuzz
+        if self % 5 == 0 && self % 3 == 0
+            "fizzbuzz"
+        elsif self % 5 == 0
+            "buzz"
+        elsif self % 3 == 0
+            "fizz"
+        else 
+            self
+        end
     end
 end
